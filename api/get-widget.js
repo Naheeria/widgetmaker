7fb374c1e5ea64ae831e9b1b540b2bbd8bb5cf35
Naheeria <soseno0809@gmail.com>
2025-11-21 19:25:26 +0900

    Update get-widget.js

--- api/get-widget.js
+++ api/get-widget.js
@@ -59,8 +59,8 @@ function setCorsHeaders(req, res) {
 const THEME_COLORS = {
     blue: { background: '#f0f4ff', border: '#a8c7ff', color: '#1a54a0' },
     green: { background: '#f0fff0', border: '#b0ffb0', color: '#38761d' },
-    // 💡 여기에 쉼표가 추가되었습니다!
     pink: { background: '#fff0f4', border: '#ffb0c7', color: '#993366' }, 
+  default: { background: 'white', border: '#ddd', color: '#333' },
     purple: { background: '#faf5ff', border: '#d4bbf9', color: '#57088b' } 
 };
 
